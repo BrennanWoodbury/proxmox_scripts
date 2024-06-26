cf4793c6f65d6d0c46d9d9f16c2d7bedaa479247
Woody <bthedub@gmail.com>
2024-06-26 15:03:12 -0600

    Create new_node_setup.sh

---
+++ kubernetes/new_node_setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+# to be used to setup a new kube node
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 hostname ip"
+  exit 1
+fi
+
+if [ $# -gt 2 ]; then
+  echo "Usage: $0 hostname ip"
+  exit 1
+fi
+
+trap ctrl_c INT
+ctrl_c() {
+  echo 'If you cancelled before rebooting, please ensure your system reboots or the ip and hostname changes will not go into effect.'
+  exit 1
+}
+
+hostname=$1
+ip=$2
+
+hostnamectl hostname $1
+sed -i 's/- 192\.168\.0\..*/$ip/24/' <file>
+
+netplan apply
+
+echo "System will reboot in 10 seconds, press Ctrl+C to cancel"
+sleep 10
+reboot
+
+
