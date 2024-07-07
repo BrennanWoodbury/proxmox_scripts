6b8d0b78a8819a8ac23586f716b282169194c516
Woody <bthedub@gmail.com>
2024-07-06 22:47:34 -0600

    Create install-cri-dockerd.sh

---
+++ kubernetes/install-cri-dockerd.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# This uses v 3.14 releaed in may of 2024, update based on your needs
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.14/cri-dockerd-0.3.14.amd64.tgz
+
+tar -xvf cri-dockerd-0.3.14.amd64.tgz
+
+mv cri-dockerd /usr/local/bin/
+
+cat <<EOF | sudo tee /etc/systemd/system/cri-docker.service
+[Unit]
+Description=CRI Interface for Docker Application Container Engine
+Documentation=https://docs.mirantis.com
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=notify
+ExecStart=/usr/local/bin/cri-dockerd --container-runtime-endpoint fd://
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+cat <<EOF | sudo tee /etc/systemd/system/cri-docker.socket
+[Unit]
+Description=CRI Docker Socket for the API
+PartOf=cri-docker.service
+
+[Socket]
+ListenStream=/run/cri-dockerd.sock
+SocketMode=0660
+SocketUser=root
+SocketGroup=docker
+
+[Install]
+WantedBy=sockets.target
+EOF
+
+
+systemctl daemon-reload
+systemctl enable cri-docker.service 
+systemctl enable cri-docker.socket
+systemctl start cri-docker.service
+systemctl start cri-docker.socket
+
+
+systemctl status cri-docker.service
